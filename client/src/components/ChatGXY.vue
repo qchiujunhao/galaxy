a67a05dbe7dade43d7b63fef964daad15c54410b
qchiujunhao <qjunhao@yahoo.com>
2026-03-18 14:56:46 -0400

    Collapse trailing assistant history on reload

--- client/src/components/ChatGXY.vue
+++ client/src/components/ChatGXY.vue
@@ -380,8 +380,20 @@ async function fetchConversation(exchangeId: string): Promise<boolean> {
     applyDatasetSelectionFromMessages(fullConversation);
     assistantMessagesToReplay.forEach((assistantMessage) => maybeRunPyodideForMessage(assistantMessage));
     if (pendingCollapsedMessages.length) {
-        pendingCollapsedMessages.forEach((msg) => messages.value.push(msg));
-        pendingCollapsedMessages.length = 0;
+        const trailingMessages = pendingCollapsedMessages.splice(0);
+        const visibleMessage = trailingMessages.pop();
+        if (visibleMessage) {
+            if (trailingMessages.length) {
+                visibleMessage.collapsedHistory = trailingMessages.map((msg) => {
+                    msg.isCollapsed = true;
+                    return msg;
+                });
+                if (visibleMessage.isCollapsed === undefined) {
+                    visibleMessage.isCollapsed = true;
+                }
+            }
+            messages.value.push(visibleMessage);
+        }
     }
 
     currentChatId.value = exchangeId;
